a6cbdda7572deea29f0649ab156c7c557264f4dc
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-12 17:01:50 +0100

    basic crud functionality and integration of stripe payment gateway

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const orderRoute = require("./routes/order")
+const cartRoute = require("./routes/cart")
 
 dotenv.config();
 
@@ -17,6 +20,9 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
+app.use("/api/product", productRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/cart", cartRoute);
 
 
 app.listen(process.env.PORT || 5000, ()=>{
